a6a3e0ec0fabee02c58b8498e60ce5a338a860a5
Adam Marshall <adam@appsynergy.net>
2016-01-02 11:07:14 +0800

    Left and right arrows now control ship facing - probably expressed as degrees for now, Elm angles later. Doesn't modulus.

---
+++ README.md
@@ -0,0 +1,1 @@
+## The classic asteroids game, implemented in Elm 0.16
--- src/Asteroids.elm
+++ src/Asteroids.elm
@@ -11,25 +11,70 @@ import Keyboard
 
 -- MODEL
 
+type alias Vector2 =
+  { x : Float
+  , y : Float
+  }
+
+type alias Ship =
+  { firing : Bool
+  , velocity : Vector2
+  , position : Vector2
+  , facing : Float
+  }
+
 type Input
   = TurnLeft
   | TurnRight
   | Thrust
   | FireWeapon
 
-type alias Ship =
-  { firing : Bool }
+type alias KeyInput =
+  { x : Int
+  , y : Int
+  }
 
 initShip : Ship
 initShip =
-  { firing = False }
+  { firing = False
+  , velocity = { x = 0, y = 0 }
+  , position = { x = 0, y = 0 }
+  , facing = 0
+  }
 
 -- UPDATE
+
+update : (Float, KeyInput) -> Ship -> Ship
+update (dt, keyInput) ship =
+  let
+    deltaFacing = toFloat keyInput.x
+  in
+  updateFacing deltaFacing ship
+
+updateFacing : Float -> Ship -> Ship
+updateFacing newFacing ship =
+  let
+    turnRate = 10
+    dF = turnRate * newFacing + ship.facing
+  in
+  { ship | facing = dF }
+
+updateShipPhysics : (Float, Float, Float) -> Ship -> Ship
+updateShipPhysics (turnLeft, turnRight, thrust) ship =
+  ship
+
 
 -- VIEW
 
 -- SIGNALS
 
-main : Element
+inputSignal : Signal (Float, KeyInput)
+inputSignal =
+  let delta = fps 30
+      tuples = Signal.map2 (,) delta Keyboard.arrows
+  in  Signal.sampleOn delta tuples
+
+
+main : Signal Element
 main =
-    show 42
+  Signal.map show (Signal.foldp update initShip inputSignal)
